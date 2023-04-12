e8b9d52af0e4852a1c2b2ff832c9272d9ab9ba8e
mariano-gomez-chavez <mariano27897@gmail.com>
2023-04-11 23:52:12 -0300

    Proyecto fundado desde initializr

---
+++ src/main/java/com/example/ecommerceSpringBootBase/EcommerceSpringBootBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceSpringBootBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringBootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringBootBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerceSpringBootBase/EcommerceSpringBootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceSpringBootBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBootBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
